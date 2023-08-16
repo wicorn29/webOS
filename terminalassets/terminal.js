d6a88479ac76c76ed9042bffb56cc9e6fbe54158
Wil Corn <wilson.corn@hotmail.com>
2023-08-15 21:27:42 -0700

    Update terminal.js

--- terminalassets/terminal.js
+++ terminalassets/terminal.js
@@ -12,11 +12,6 @@ inputElement.addEventListener('keydown', (event) => {
 
 function executeCommand(command) {
   const response = runCommand(command); // Replace with your command processing logic
-  if (typeof response === 'function') {
-    const functionResponse = response();
-    outputElement.textContent += `\n> ${command}\n${functionResponse}\n`;
-  } else {
-    outputElement.textContent += `\n> ${command}\n${response}\n`;
-  }
+  outputElement.textContent += `\n> ${command}\n${response}\n`;
   outputElement.scrollTop = outputElement.scrollHeight;
 }
