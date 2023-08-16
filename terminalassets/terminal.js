e9221fea5b87418f757b472a54136893cc1b1586
Wil Corn <wilson.corn@hotmail.com>
2023-08-15 21:24:50 -0700

    Update terminal.js

--- terminalassets/terminal.js
+++ terminalassets/terminal.js
@@ -1,17 +1,10 @@
-const inputElement = document.getElementById('input');
-const outputElement = document.getElementById('output');
-
-inputElement.addEventListener('keydown', (event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault();
-    const command = inputElement.value;
-    executeCommand(command);
-    inputElement.value = '';
-  }
-});
-
 function executeCommand(command) {
   const response = runCommand(command); // Replace with your command processing logic
-  outputElement.textContent += `\n> ${command}\n${response}\n`;
+  if (typeof response === 'function') {
+    const functionResponse = response();
+    outputElement.textContent += `\n> ${command}\n${functionResponse}\n`;
+  } else {
+    outputElement.textContent += `\n> ${command}\n${response}\n`;
+  }
   outputElement.scrollTop = outputElement.scrollHeight;
 }
