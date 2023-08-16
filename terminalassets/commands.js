848b85930fb140c8913628528a7c5f303f08cfb6
Wil Corn <wilson.corn@hotmail.com>
2023-08-15 21:48:34 -0700

    Update commands.js

--- terminalassets/commands.js
+++ terminalassets/commands.js
@@ -10,9 +10,4 @@ registerCommand('about', 'Display information about the OS', () => {
   // Logic to generate the about response
 });
 
-// Register the download command
-registerCommand('download', 'Download a file from URL', () => {
-  // Logic to generate the download response
-});
-
 // Define and register more commands here
