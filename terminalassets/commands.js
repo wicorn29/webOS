b5a5714f8216d336ccb32843f3699bee83986879
Wil Corn <wilson.corn@hotmail.com>
2023-08-15 21:23:51 -0700

    Update commands.js

--- terminalassets/commands.js
+++ terminalassets/commands.js
@@ -1,65 +1,8 @@
 const commands = {
-  help: 'Available commands: help, about, ls, pwd, cd, mkdir, touch, cat, clear',
-  about: 'This is your simple web-based OS terminal.',
-  ls: 'List files and directories',
-  pwd: 'Print current working directory',
-  cd: 'Change directory',
-  mkdir: 'Create a new directory',
-  touch: 'Create a new file',
-  cat: 'Display file content',
-  clear: 'Clear the terminal'
+  help: () => {
+    const availableCommands = Object.keys(commands).join(', ');
+    return `Available commands: ${availableCommands}`;
+  },
+  about: 'This is your simple web-based OS terminal.'
+  // ... other commands ...
 };
-
-// Implement the logic for each command
-function runCommand(command) {
-  const parts = command.split(' ');
-  const cmd = parts[0];
-  const args = parts.slice(1);
-
-  switch (cmd) {
-    case 'ls':
-      return listFiles();
-    case 'pwd':
-      return getCurrentDirectory();
-    case 'cd':
-      return changeDirectory(args[0]);
-    case 'mkdir':
-      return createDirectory(args[0]);
-    case 'touch':
-      return createFile(args[0]);
-    case 'cat':
-      return displayFileContent(args[0]);
-    case 'clear':
-      return clearTerminal();
-    default:
-      return `Command not found: ${cmd}`;
-  }
-}
-
-function listFiles() {
-  // Logic to list files and directories
-}
-
-function getCurrentDirectory() {
-  // Logic to get current working directory
-}
-
-function changeDirectory(path) {
-  // Logic to change directory
-}
-
-function createDirectory(name) {
-  // Logic to create a new directory
-}
-
-function createFile(name) {
-  // Logic to create a new file
-}
-
-function displayFileContent(name) {
-  // Logic to display file content
-}
-
-function clearTerminal() {
-  // Logic to clear the terminal
-}
