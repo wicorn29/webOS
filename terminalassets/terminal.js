9e7bcfd161a68e73381dffda11ad0fb6483e7e15
Wil Corn <wilson.corn@hotmail.com>
2023-08-15 21:41:02 -0700

    Update terminal.js

--- terminalassets/terminal.js
+++ terminalassets/terminal.js
@@ -1,15 +1,17 @@
-// terminal.js
+const inputElement = document.getElementById('input');
+const outputElement = document.getElementById('output');
+
+inputElement.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    const command = inputElement.value;
+    executeCommand(command);
+    inputElement.value = '';
+  }
+});
 
 function executeCommand(command) {
-  const args = command.split(' ');
-  const cmd = args.shift().toLowerCase();
-
-  if (commands.hasOwnProperty(cmd)) {
-    const response = commands[cmd](args.join(' '));
-    outputElement.textContent += `\n> ${command}\n${response}\n`;
-    outputElement.scrollTop = outputElement.scrollHeight;
-  } else {
-    outputElement.textContent += `\n> ${command}\nCommand not found: ${cmd}\n`;
-    outputElement.scrollTop = outputElement.scrollHeight;
-  }
+  const response = runCommand(command); // Replace with your command processing logic
+  outputElement.textContent += `\n> ${command}\n${response}\n`;
+  outputElement.scrollTop = outputElement.scrollHeight;
 }
